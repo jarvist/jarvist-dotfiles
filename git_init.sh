f043edbb5dd48f43ee17bac665b091d331bdbba9
Jarvist Moore Frost <jarvist@fakemail.fake>
2012-12-01 22:10:14 -0500

    Getting there...

---
+++ git_init.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Jarvist Moore Frost"
+git config --global user.email "jarvist@fakemail.fake"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+

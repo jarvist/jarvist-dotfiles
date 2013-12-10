0ecec3d84b22e3d6f8cb19570446afaf46634f58
Jarvist Moore Frost <jarvist@fakemail.fake>
2013-12-10 15:27:45 +0000

    Adds http://dx.doi.org to dois in input stream. For emailing links to papers.

--- scripts/add_http_dx_org_sed.sh
+++ scripts/add_http_dx_org_sed.sh
@@ -1,1 +1,9 @@
-sed s#doi:#http://dx.doi.org/#g
+!/bin/sh
+# Mini-script to turn dois: exported from Zotero 'generate a bibliography' into clickable http links
+# For emailing around Journal Club bibliographies
+
+# Reminder: Preferred form is Chcagi Manual of Style (full note)
+# (*) Bibliography
+# Copy to clipboard
+
+sed "s#doi:#http://dx.doi.org/#g"

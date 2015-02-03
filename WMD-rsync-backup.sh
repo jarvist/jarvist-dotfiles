8693c51eff1d96d81c593722b8f5ff03a9c86f2c
Jarvist Moore Frost <jarvist@gmail.com>
2015-02-03 16:23:02 +0000

    Comments!

--- WMD-rsync-backup.sh
+++ WMD-rsync-backup.sh
@@ -3,6 +3,7 @@
 # Rsync WMD backup script
 # 2015-02-02 - JMF; First Version
 
+# Construct an exclude.tmp list for rsync
 cat > exclude.tmp <<EOF
 WAVECAR
 CHG
@@ -10,11 +11,22 @@ CHGCAR
 *.chk
 EOF
 
+# Echo the contents to the user
 echo "I am excluding from backup: "
 cat exclude.tmp
 
+# OK; loop over the machines from which we want to backup
+#  --> these are alised in your .ssh/config
 for MACHINE in ultra archer neon scandium titanium
 do
+    # Banner display of which MACHINE we're hoovering from
     figlet "${MACHINE}"
+    # Rsync
+    #  -a(rchive)
+    #  -v(erbose)
+    #  --exclude-from - dynamically generated list
+    #  Pull down MACHINE:~/work/ into a directory called MACHINE-work
+    # && logical and, so only if rsync exited happily, 
+    # touch - update timestamped note of backupattempt
     rsync -av --exclude-from 'exclude.tmp' "${MACHINE}:~/work/" ./${MACHINE}-work && touch ${MACHINE}-work_lastbackupattempt
 done

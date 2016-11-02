d8ced411642591f0974589304b840dc80f35a80e
Jarvist Moore Frost <jarvist@gmail.com>
2016-11-02 10:13:53 +0000

    Added internet radio (Soma, BBC R4,R6).

--- MACOSX_profile.sh
+++ MACOSX_profile.sh
@@ -95,6 +95,15 @@ alias play-pinknoise="play -n -n --combine merge synth pinknoise band -n 1200 18
 alias play-ocean="play -n -n --combine merge synth pinknoise band -n 1200 1800 tremolo 50 10 tremolo 0.14 70 tremolo 0.2 50 gain  -10"
 alias play-enterprise="play -c2 -n synth whitenoise band -n 100 24 band -n 300 100 gain +20"
 
+# Internet Radios
+# Soma FM
+alias soma-dronezone='mplayer -playlist http://somafm.com/dronezone130.pls'
+alias soma-groovesalad='mplayer -playlist http://somafm.com/groovesalad130.pls'
+# BBC: What else do you need other than R4 and R6?  :)
+# Latest working playlists from: https://gist.github.com/noodlebug/0e5e3754f4e8dbf608e72431b9c34484
+alias r6='mplayer -playlist http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/low/ak/bbc_6music.m3u8 '
+alias r4=' mplayer -playlist http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/low/ak/bbc_radio_fourfm.m3u8 '
+
 # set PATH so it includes user's private bin if it exists
 if [ -d ~/bin ] ; then
  PATH=~/bin:"${PATH}"
